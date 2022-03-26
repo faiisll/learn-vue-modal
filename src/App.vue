482c90f0492d846f57dd008d1ae64a894ad71707
faiisll <faisal.ayas72@gmail.com>
2022-03-26 12:07:39 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input ref="name" type="text" />
+  <button @click="toggleModal">Toggle Modal</button>
+  <div v-if="showModal">
+    <Modal theme="danger" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Submit</a>
+        <a href="#">Close</a>
+      </template>
+      <h1>Ninja</h1>
+      <p>Ninja paragraph</p>
+    </Modal>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "My First Vue via CLI",
+      header: "header by props",
+      showModal: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{danger: theme === 'danger'}">
+      <slot></slot>
+      <div class="action">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.modal.danger {
+  background: tomato;
+}
+
+.backdrop {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+</style>
